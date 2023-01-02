e9382ae9d1b929485d74db6ea8d124de72519386
wesleyfrd <46203701+wesleyfrd@users.noreply.github.com>
2023-01-01 23:19:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,223 +1,61 @@
-// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
-// ╠╩╗║╣ ║║║ ║ ║ ║
-// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
-// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
-// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
-// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+ // General
+  name: 'Wesley',
+  imageBackground: false,
+  openInNewTab: true,
 
-const CONFIG = {
-	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-	// ├┴┐├─┤└─┐││  └─┐
-	// └─┘┴ ┴└─┘┴└─┘└─┘
-
-	// General
-	name: 'Wesley',
-	imageBackground: false,
-	openInNewTab: true,
-	twelveHourFormat: false,
-
-	// Greetings
-	greetingMorning: 'Bom dia!',
-	greetingAfternoon: 'Boa tarde,',
-	greetingEvening: 'Boa tarde,',
-	greetingNight: 'Boa noite!',
-
-	// Layout
-	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
-
-	// Weather
-	weatherKey: '1a1d7f3f6e0e69a208939eddd1402d04', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
-
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '-23.535250',
-	defaultLongitude: '-46.582670',
-
-	// Autochange
-	autoChangeTheme: true,
-
-	// Autochange by OS
-	changeThemeByOS: true,
-
-	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
-
-	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-	// ├┴┐│ │ │  │ │ ││││└─┐
-	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
-
-	firstButtonsContainer: [
-		{
-			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
-		},
-		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
-		},
-		{
-			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
-		},
-		{
-			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
-		},
-		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
-		},
-	],
-
-	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
-		},
-		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
-		{
-			id: '3',
-			name: 'bot',
-			icon: 'bot',
-			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
-			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
-		},
-		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
-		},
-	],
-
-
-	// ┬  ┬┌─┐┌┬┐┌─┐
-	// │  │└─┐ │ └─┐
-	// ┴─┘┴└─┘ ┴ └─┘
-
-	// First Links Container
-	firstlistsContainer: [
-		{
-			icon: 'coffee',
-			id: '1',
-			links: [
-				{
-					name: 'Sharepoint',
-					link: 'https://projectplancombr.sharepoint.com',
-				},
-				{
-					name: 'Outlook',
-					link: 'outlook.office.com',
-				},
-			],
-		},
-		{
-			icon: 'coffee',
-			id: '2',
-			links: [
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Youtube',
-					link: 'https://www.youtube.com',
-				},
-				{
-					name: 'Facebook',
-					link: 'https://www.facebook.com',
-				},
-				{
-					name: 'Instagram',
-					link: 'https://www.instagram.com',
-				},
-			],
-		},
-	],
-
-	// Second Links Container
-	secondListsContainer: [
-		{
-			icon: 'coffee',
-			id: '1',
-			links: [
-				{
-					name: 'AwesomePiracy',
-					link: 'https://github.com/Igglybuff/awesome-piracy',
-				},
-				{
-					name: 'Audiobooks',
-					link: 'https://audiobookbay.li',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Libgen',
-					link: 'https://libgen.fun',
-				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
-				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
-				},
-				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
-				},
-				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
-				},
-			],
-		},
-	],
-};
+  // Greetings
+  greetingMorning: 'Bom dia!',
+  greetingAfternoon: 'Boa tarde,',
+  greetingEvening: 'Boa tarde,',
+  greetingNight: 'Boa noite,',
+  bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+	  
+	  
+    firstlistsContainer: [
+    {
+      icon: 'music',
+      id: '1',
+      links: [
+        {
+          name: 'Inspirational',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Classic',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Oldies',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Rock',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+      ],
+    },
+    {
+      icon: 'coffee',
+      id: '2',
+      links: [
+        {
+          name: 'Linkedin',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Dribbble',
+          link: 'https://www.linkedin.com',
+        },
+        {
+          name: 'Trello',
+          link: 'https://www.trello.com',
+        },
+        {
+          name: 'Slack',
+          link: 'https://www.slack.com',
+        },
+      ],
+    },
+  ],
+	 
+	 
